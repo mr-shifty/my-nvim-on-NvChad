e539b60d83d8f2dc69b75ca498e6e315ffc55e8c
Pavel Melnikov <room.job@mail.ru>
2024-02-01 16:05:16 +0300

    Initial commit.

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,28 @@
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+        return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "pyright",
+        "mypy",
+        "ruff",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+    },
+  }
+return plugins
