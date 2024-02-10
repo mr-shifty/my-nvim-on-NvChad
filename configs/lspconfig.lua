c6f206b1589f31ab523f5f6da62bae9a61c51ae6
Pavel Melnikov <room.apps@mail.ru>
2024-02-10 18:26:48 +0300

    Update lsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,14 +1,16 @@
 local config = require("plugins.configs.lspconfig")
 
-local on_attach = config.on_attach
--- local capabilities = config.capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+-- local on_attach = config.on_attach
+
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+
+local capabilities = config.capabilities
 
 local lspconfig = require("lspconfig")
 
 lspconfig.pyright.setup({
-  on_attach = on_attach,
+  -- on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"python"}
 })
