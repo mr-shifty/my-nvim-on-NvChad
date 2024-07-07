8faa5be4ebb43eca76f46ce0e68542db0f4ed693
Pavel <room.job@mail.ru>
2024-07-07 22:41:07 +0300

    Fix nvim-tree root_folder_label

--- lua/configs/overrides.lua
+++ lua/configs/overrides.lua
@@ -33,11 +33,12 @@ M.treesitter = {
 M.nvimtree = {
 
   renderer = {
-    root_folder_label = function(path)
-      return "" .. vim.fn.fnamemodify(path, ":pwd")
-    end,
-    -- root_folder_label = ":~:s?$?/..?",
-    highlight_git = true,
+    -- root_folder_label = "true",
+    -- root_folder_label = function(path)
+    --   return "" .. vim.fn.fnamemodify(path, ":pwd")
+    -- end,
+    root_folder_label = ":~:s?$?/..?",
+    highlight_git = "name",
     indent_markers = {
       enable = true,
     },
